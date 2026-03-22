c4cbe987f763a04beed2edfd14bfa4caa8378721
Mohd Rashid <siddrashid1998@outlook.com>
2026-03-22 21:47:49 +0530

    Disable recipe button while loading

--- components/IngredientsList.jsx
+++ components/IngredientsList.jsx
@@ -11,7 +11,10 @@ export default function IngredientsList(props) {
                     <h3>Ready for a recipe?</h3>
                     <p>Generate a recipe from your list of ingredients.</p>
                 </div>
-                <button onClick={props.getRecipe}>
+                <button
+                    onClick={props.getRecipe}
+                    disabled={props.isLoading}
+                >
                     {props.isLoading ? "Getting recipe..." : "Get a recipe"}
                 </button>
             </div>}
